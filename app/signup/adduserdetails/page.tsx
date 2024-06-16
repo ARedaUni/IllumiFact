332ca42ad45e96fe1f351950f645798bdce26680
Ali Reda <w1821480@my.estminster.ac.uk>
2024-06-16 10:44:00 +0100

    removed redundant comments on adduserdetails component

--- app/signup/adduserdetails/page.tsx
+++ app/signup/adduserdetails/page.tsx
@@ -1,42 +1,7 @@
 "use client";
-import { sessionType } from "@/Types/allTypes";
 import { createClient } from "@/utils/supabase/supabase";
 import { User } from "@supabase/supabase-js";
-import { useRouter } from "next/router";
-//import { createClient } from "@/utils/supabase/server";
-//import { redirect } from "next/navigation";
 import { ChangeEvent, MouseEventHandler, useEffect, useState } from "react";
-// export async function uploadToDatabase(
-//   user,
-//   formData: FormData
-// ) {
-//   "use server";
-//   const username = formData.get("username") as string;
-//   const image = formData.get("profile-picture");
-//   const supabase = createClient()
-//   async function uploadImageToSupabaseBucket(bucketName: string, imageName: string, image: File|null|any) {
-//     const { data, error } = await supabase.storage
-//       .from(bucketName)
-//       .upload(imageName, image, {
-//         cacheControl: "3600",
-//         upsert: false,
-//       });
-//   };
-//   if (image && image instanceof File && image.name !== "" && username !== "") {
-//     const { error } = await supabase
-//       .from("users")
-//       .update({ username: username, pfp: image.name })
-//       .eq("id", user.id);
-
-//     if (error) {
-//       console.error("Error updating user data:", error);
-//       return;
-//     }
-
-//     await uploadImageToSupabaseBucket("userprofilepictures", image.name, image);
-
-//   }
-// }
 
 export default function adduserdetails() {
   const supabase = createClient();
